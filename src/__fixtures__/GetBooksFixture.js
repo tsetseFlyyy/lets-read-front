3e5c2c9d8f321af278fb55fa0fb6673fc6b7fc2c
Shalkar Koshenayev <shalkar.koshenayev@gmail.com>
2023-03-09 23:46:39 +0600

    bookstable test with getbooks fixture

---
+++ src/__fixtures__/GetBooksFixture.js
@@ -0,0 +1,17 @@
+export const books = [
+	{
+	  author: "Author1",
+	  title: "Book1",
+	  friendsbook: false,
+	},
+	{
+	  author: "Author2",
+	  title: "Book2",
+	  friendsbook: false,
+	},
+	{
+	  author: "Author3",
+	  title: "Book3",
+	  friendsbook: false,
+	},
+ ];
--- src/__tests__/booksTable.spec.js
+++ src/__tests__/booksTable.spec.js
@@ -1,44 +1,21 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { useGetBooksQuery } from "../services/redux/API/booksAPI"; // RTK Query API
-import { Component } from "../components/Component";
 import { BooksTable } from "../components/BooksTable";
 
 import * as rtkQuery from "../services/redux/API/booksAPI";
+import { books } from "../__fixtures__/GetBooksFixture";
 
 jest.mock("../services/redux/API/booksAPI");
 
-const mockData = [
-  {
-    author: "author1",
-    title: "my book1",
-    friendsbook: false,
-  },
-  {
-    author: "author2",
-    title: "my book2",
-    friendsbook: false,
-  },
-  {
-    author: "author3",
-    title: "my book3",
-    friendsbook: false,
-  },
-];
-
 describe("MyComponent", () => {
   it("should render data after API request", () => {
-    jest
-      .spyOn(rtkQuery, "useGetBooksQuery")
-      .mockReturnValue({ data: mockData });
+    jest.spyOn(rtkQuery, "useGetBooksQuery").mockReturnValue({ data: books });
     const tree = render(<BooksTable />);
 
-    // Check that loading state is not displayed
-    // expect(screen.queryByText("Loading...")).toBeNull();
-    expect(tree).toMatchSnapshot();
-    // Check that data is displayed correctly
-    // expect(screen.getByText("author")).toBeInTheDocument();
-    // expect(screen.getByText("my book")).toBeInTheDocument();
+    expect(tree).toMatchSnapshot(); // Снимок
+    for (let i = 1; i <= 3; i++) {
+      expect(screen.getByText(`Author${i}`)).toBeInTheDocument();
+      expect(screen.getByText(`Book${i}`)).toBeInTheDocument();
+    }
   });
 });
