f28fb1fef8346bb0f0bab1a5cfc7e6f720f28ebb
Shalkar Koshenayev <shalkar.koshenayev@gmail.com>
2023-03-12 19:22:57 +0600

    share test is done

--- src/services/__tests__/books.test.js
+++ src/services/__tests__/books.test.js
@@ -29,10 +29,11 @@ import { body } from "../__fixtures__/ShareBookFixture";
 it("Share my book by ID", async () => {
   const response = new Books();
   var data = await response.get();
-  const id = "640dc8e590978bc56f47c44b";
-  data.map((book) => {
+  const id = "640dd1854bd84f29d5a2e63e";
+  let index = 0;
+  data.map((book, i) => {
     if (
-      (book._id != id && book.friendsbook != false || book.name,
+      (book._id != id || book.friendsbook != false || book.name,
       book.surname,
       book.patronymic != "")
     ) {
@@ -40,7 +41,16 @@ it("Share my book by ID", async () => {
     }
   });
   await response.sendToFriend(id, body);
-
-  console.warn(data);
-  expect(data).not.toBeUndefined();
+  data = await response.get();
+  data.map((book, i) => {
+    if (book._id == id) {
+      index = i;
+      console.warn(i);
+      console.warn(index);
+    }
+  });
+  expect(data[index].name).toBe(body.name);
+  expect(data[index].surname).toBe(body.surname);
+  expect(data[index].patronymic).toBe(body.patronymic);
+  expect(data[index].deadline).toBe(body.deadline);
 });
